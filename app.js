06f16d0ccb9128007c56ee7157e23fc8374536cf
EssaadaniYounes <younesessaadani2003@gmail.com>
2022-12-15 21:37:21 +0100

    create user

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.set("views", __dirname + "/views");
 // JSONPARSER
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use('/public', express.static('public'));
+
 
 app.get("/", async function (req, res) {
   res.render("index", { users: users });
@@ -18,16 +20,20 @@ app.get("/users/user/:id", async (req, res) => {
   res.render('detail',{user:user});
 
 });
+app.get('/create-user',async(req,res)=>{
+    res.render('create-user');
+})
 app.post("/", async (req, res) => {
   const user = req.body;
   user.id = users[users.length - 1].id + 1;
 
   users.push(user);
 
-  res.send({
-    message: "user created",
-    data: user,
-  });
+//   res.send({
+//     message: "user created",
+//     data: user,
+//   });
+  res.render("index", { users: users });
 });
 
 app.listen(8000, console.log("app runing"));
